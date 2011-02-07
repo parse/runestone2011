788c5a68d4f97e701076d15843e6e05996130c8b
anders.hassis <anders.hassis@06114496-cf75-458a-ad0c-944fd1120a3f>
2011-02-07 12:54:26 +0000

    Resolving...

--- Test.java
+++ Test.java
@@ -4,16 +4,22 @@ import lejos.nxt.*;
         public static void main (String[] args) {
           System.out.println("Hello World");
           MehMeh test = new MehMeh();
-            Motor.B.setSpeed(100);
-            Motor.C.setSpeed(100);
+            //Motor.B.setSpeed(100);
+            //Motor.C.setSpeed(100);
             
             LCD.drawString("FORWARD", 0, 0);
+            rotateInPlace(100);
+            Motor.A.rotate(180,true);
             while (true) {
-              test.forward();
-              
+               
               if (Button.ESCAPE.isPressed())
                 break;
             }
 
+        }
+        
+        public static void rotateInPlace (int degree) {
+          Motor.C.rotate(degree,true);
+          Motor.B.rotate(-degree,true);
         }
       }
