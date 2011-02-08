4489f1a1d60e60dcb1d3cfe69dbf1facd68359fa
JuliusS <JuliusS@06114496-cf75-458a-ad0c-944fd1120a3f>
2011-02-08 12:29:09 +0000

    fixed tabsize

--- WallE.java
+++ WallE.java
@@ -1,24 +1,23 @@
 import lejos.nxt.*;
 
 public class WallE {
-  public static void main (String[] args) {
-    
-      rotateInPlace(100);
-      Motor.A.rotate(180,true);
+	public static void main (String[] args) {
+		rotateInPlace(100);
+		Motor.A.rotate(180,true);
       
-      // Start "game loop"
-      while (true) {
+		// Start "game loop"
+		while (true) {
         
-        // Handle termination of program
-        if (Button.ESCAPE.isPressed()) {
-          break;
-        }
-      }
-  }
-  
-  public static void rotateInPlace (int degree) {
-    LCD.drawString("ROTATE IN PLACE", 0, 0);
-    Motor.C.rotate(degree,true);
-    Motor.B.rotate(-degree,true);
-  }
+        	// Handle termination of program
+        	if (Button.ESCAPE.isPressed()) {
+        		break;
+        	}
+		}
+	}
+	
+	public static void rotateInPlace (int degree) {
+    	LCD.drawString("ROTATE IN PLACE", 0, 0);
+    	Motor.C.rotate(degree,true);
+    	Motor.B.rotate(-degree,true); 
+	}
 }
