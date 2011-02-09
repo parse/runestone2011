aa57ad3c7ff9817df267fd69c694601c2a9420dd
JuliusS <JuliusS@06114496-cf75-458a-ad0c-944fd1120a3f>
2011-02-09 19:37:36 +0000

    added some test code for turning the robot in place and simple AI using the ultrasonic sensor

--- WallE.java
+++ WallE.java
@@ -2,22 +2,93 @@ import lejos.nxt.*;
 
 public class WallE {
 	public static void main (String[] args) {
-		rotateInPlace(100);
-		Motor.A.rotate(180,true);
+		//rotateInPlace(1000);
+		//Motor.A.rotate(360,true);
+		//rotateUltra();
       
+      
+      	UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
+
+    /*while (!Button.ESCAPE.isPressed()) {
+      LCD.clear();
+      LCD.drawString(sonic.getVersion(), 0, 0);
+      LCD.drawString(sonic.getProductID(), 0, 1);
+      LCD.drawString(sonic.getSensorType(), 0, 2);
+      LCD.drawInt(sonic.getDistance(), 0, 3);
+      try{Thread.sleep(1000);}catch(Exception e) {}
+    }*/
 		// Start "game loop"
 		while (true) {
+			Motor.B.forward();
+			Motor.C.forward();
+			try{Thread.sleep(1000);}catch(Exception e) {}	//Sleep 1 sec
+			Motor.B.stop();
+			Motor.C.stop();
+			
+			//Check the distance in 3 direction and go in the way with the most space
+			int a1, a2, a3 = 0;
+			Motor.A.rotateTo(-90);
+			try{Thread.sleep(1000);}catch(Exception e) {}
+			a1 = sonic.getDistance();
+			if(a1 < 83) {
+				Sound.beep();
+			}
+			Motor.A.rotateTo(0);
+			try{Thread.sleep(1000);}catch(Exception e) {}
+			a2 = sonic.getDistance();
+			if(a2 < 83) {
+				Sound.beep();
+			}
+			Motor.A.rotateTo(90);
+			try{Thread.sleep(1000);}catch(Exception e) {}
+			a3 = sonic.getDistance();
+			if(a3 < 83) {
+				Sound.beep();
+			}
+			
+			if(a1 >= a2 && a1 >= a3) {
+				rotateInPlace(-251);
+			}
+			else if(a3 >= a1 && a3 >= a2) {
+				rotateInPlace(251);
+			}
+			//driveForward(1000);
+			//rotateInPlace(503);
         
         	// Handle termination of program
         	if (Button.ESCAPE.isPressed()) {
         		break;
         	}
-		}
+		} 
+		LCD.drawString("EXIT", 0, 0);
 	}
 	
 	public static void rotateInPlace (int degree) {
     	LCD.drawString("ROTATE IN PLACE", 0, 0);
     	Motor.C.rotate(degree,true);
-    	Motor.B.rotate(-degree,true); 
+    	Motor.B.rotate(-degree); 
+	}
+	
+	public static void driveForward (int degree) {
+		LCD.drawString("DRIVE FORWARD", 0, 0);
+		Motor.C.rotate(degree, true);
+    	Motor.B.rotate(degree); 
+	}
+	
+	public static void rotateUltra () {
+		Motor.A.rotateTo(0);
+		try{Thread.sleep(1000);}catch(Exception e) {}
+		Motor.A.rotateTo(90);
+		try{Thread.sleep(1000);}catch(Exception e) {}
+		Motor.A.rotateTo(180);
+		try{Thread.sleep(1000);}catch(Exception e) {}
+		Motor.A.rotateTo(270);
+		try{Thread.sleep(1000);}catch(Exception e) {}
+		Motor.A.rotateTo(180);
+		try{Thread.sleep(1000);}catch(Exception e) {}
+		Motor.A.rotateTo(90);
+		try{Thread.sleep(1000);}catch(Exception e) {}
+		Motor.A.rotateTo(0);
+		try{Thread.sleep(1000);}catch(Exception e) {}
 	}
 }
